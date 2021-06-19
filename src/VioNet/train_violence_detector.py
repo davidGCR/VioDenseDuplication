d0273574aff3d9072e793867d99f7142c554a927
davidGCR <dchoqueluque@gmail.com>
2021-06-19 15:13:26 -0500

    fixed training

--- src/VioNet/train_violence_detector.py
+++ src/VioNet/train_violence_detector.py
@@ -1,4 +1,5 @@
 
+from torch._C import device
 from torch.utils.tensorboard import SummaryWriter
 import torch
 
@@ -14,45 +15,64 @@ from models.anomaly_detector import custom_objective, RegularizedLoss
 from epoch import train_regressor
 from utils import Log, save_checkpoint, load_checkpoint
 from utils import get_torch_device
-device = get_torch_device()
+# device = get_torch_device()
 
 def main(config: Config):
-    make_dataset = MakeRWF2000(root='/Users/davidchoqueluqueroman/Documents/DATASETS_Local/RWF-2000/frames', 
+    device = config.device
+    make_dataset = MakeRWF2000(root='/media/david/datos/Violence DATA/RWF-2000/frames',#'/Users/davidchoqueluqueroman/Documents/DATASETS_Local/RWF-2000/frames', 
                                 train=True,
-                                path_annotations='/Users/davidchoqueluqueroman/Documents/DATASETS_Local/Tubes/RWF-2000')
+                                path_annotations='/media/david/datos/Violence DATA/Tubes/RWF-2000')#'/Users/davidchoqueluqueroman/Documents/DATASETS_Local/Tubes/RWF-2000')
     dataset = TubeDataset(frames_per_tube=16, 
                             min_frames_per_tube=8, 
                             make_function=make_dataset,
                             spatial_transform=transforms.Compose([
                                 transforms.CenterCrop(224),
                                 transforms.ToTensor()
-                            ]))
+                            ]),
+                            max_num_tubes=8)
     loader = DataLoader(dataset,
                         batch_size=4,
-                        shuffle=True,
+                        shuffle=False,
                         num_workers=0,
                         pin_memory=True,
                         collate_fn=my_collate)
 
     model, params = ViolenceDetector_model(config, device)
-    print(model)
+    # print(model)
+    optimizer = torch.optim.Adadelta(params, lr=config.learning_rate, eps=1e-8)
+
+    criterion = RegularizedLoss(model, custom_objective)
     
     ##iterate over dataset
     for i, data in enumerate(loader):
         # path, label, annotation,frames_names, boxes, video_images = data
         boxes, video_images, labels = data
+        # boxes, video_images, labels = boxes.to(device), video_images.to(device), labels.to(device)
         print('_____ {} ______'.format(i+1))
-        # print('path: ', path)
-        # print('label: ', label)
-        # print('annotation: ', annotation)
+        
         print('boxes: ', type(boxes), len(boxes), '-boxes[0]: ', boxes[0].size())
         print('video_images: ', type(video_images), len(video_images), '-video_images[0]: ', video_images[0].size())
         print('labels: ', type(labels), len(labels), '-labels: ', labels)
+
+        video_images = [torch.rand(v.size()[0],3,16,224,224) for v in video_images]
+        scores = []
+        for i in range(len(video_images)):
+            video_images[i] = video_images[i].to(device)
+            boxes[i] = boxes[i].to(device)
+            # labels[i] = labels[i].to(device)
+
+            print('boxes[i]: ', boxes[i].size())
+            print('video_images[i]: ', video_images[i].size())
+            print('labels: ', labels[i])
+
+            y_pred = model(video_images[i], boxes[i])
+            scores.append(y_pred)
+        print('Scores: ', scores)
 
 if __name__=='__main__':
     config = Config(
         model='',
         dataset='',
-        device=''
+        device=get_torch_device()
     )
     main(config)
