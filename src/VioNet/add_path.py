5aaa88759fca3d3a76c64ad7cb6431085649a45b
davidGCR <dchoqueluque@gmail.com>
2021-08-10 11:32:35 -0500

    added add_path

--- src/VioNet/add_path.py
+++ src/VioNet/add_path.py
@@ -8,7 +8,7 @@ def add_path(path):
 this_dir = os.path.dirname(__file__)
 lib_path = os.path.join(this_dir, '..')
 
-print('this_dir: ', this_dir)
-print('lib_path: ', lib_path)
+# print('this_dir: ', this_dir)
+# print('lib_path: ', lib_path)
 
 add_path(os.path.normpath(lib_path))
